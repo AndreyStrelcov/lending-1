c3b7940c874f9ef5bef8dd83e11ba8bed6afaee9
Andrey Strelcov <astrelcov@yahoo.com>
2017-07-09 10:19:44 +0300

    mobile list

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
-// (function($){
-//   $(function() {
-//     $('.menu-block--toggle').on('click', function() {
-//       $(this).closest('.menu-block').toggleClass('menu_state_open');
-//     });
-//   });
-// })(jQuery);
+(function($){
+  $(function() {
+    $('.menu-block--toggle').on('click', function() {
+      $(this).closest('.menu-block').toggleClass('menu_state_open');
+    });
+  });
+})(jQuery);
