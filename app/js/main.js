950575de040f88e73cea0215a46414b4330b0016
Andrey Strelcov <astrelcov@yahoo.com>
2018-01-06 13:33:26 +0200

    new name

--- app/js/main.js
+++ app/js/main.js
@@ -20,3 +20,15 @@ $(window).on("scroll", function() {
 $btnTop.on("click", function() {
   $("html,body").animate({scrollTop:0}, 900)
 });
+// Map
+function initMap() {
+  var uluru = {lat: 48.4692332, lng: 38.8114223};
+  var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: uluru
+  });
+  var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+  });
+}
